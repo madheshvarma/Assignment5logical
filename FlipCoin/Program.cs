e3853288ec1fee7172f7c01dd4a713b351c4bffd
madheshvarma <madheshvarma369@gmail.com>
2023-01-29 11:33:37 +0530

    flipcoin

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFlips;
+            int heads = 0;
+            int tails = 0;
+
+            Console.WriteLine("Enter the number of times to flip the coin: ");
+            numFlips = Convert.ToInt32(Console.ReadLine());
+
+            if (numFlips < 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+            }
+            else
+            {
+                Random rnd = new Random();
+
+                for (int i = 0; i < numFlips; i++)
+                {
+                    double coin = rnd.NextDouble();
+                    if (coin < 0.5)
+                    {
+                        tails++;
+                    }
+                    else
+                    {
+                        heads++;
+                    }
+                }
+
+                double headPercent = (heads * 100.0) / numFlips;
+                double tailPercent = (tails * 100.0) / numFlips;
+
+                Console.WriteLine("Heads: " + headPercent + "%");
+                Console.WriteLine("Tails: " + tailPercent + "%");
+            }
+        }
+    }
+}
