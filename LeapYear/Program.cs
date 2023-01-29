c60d0bbba92ea5373e17b2af0022f9a54f449160
madheshvarma <madheshvarma369@gmail.com>
2023-01-29 12:31:33 +0530

    lepayear

---
+++ LeapYear/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            // Check if year is a 4-digit number
+            if (year < 1000 || year > 9999)
+            {
+                Console.WriteLine("Invalid input. Please enter a 4-digit year.");
+                return;
+            }
+
+            // Determine if year is a leap year
+            bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+            if (isLeapYear)
+            {
+                Console.WriteLine(year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year.");
+            }
+        }
+    }
+}
+
+
+
+
