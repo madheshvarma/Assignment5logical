b76c03b3b43fc53861ca3847c02ee584240c5669
madheshvarma <madheshvarma369@gmail.com>
2023-01-29 16:35:17 +0530

    s

---
+++ powerof2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace powerof2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, p = 1;
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter a value");
+            if (num > 31)
+            {
+                Console.WriteLine("number should be less than given");
+                return;
+            }
+            else
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    p = p * 2;
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+}
